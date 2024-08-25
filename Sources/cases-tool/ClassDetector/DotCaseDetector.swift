f1155460b61147dbd5feb36942ab6c2a1461907a
swiftproger <swiftapp@mail.ru>
2024-08-25 22:51:50 +0300

    create method hasValidSeparatorStructure
    refactoring DotCaseDetector

--- Sources/cases-tool/ClassDetector/DotCaseDetector.swift
+++ Sources/cases-tool/ClassDetector/DotCaseDetector.swift
@@ -3,19 +3,12 @@
 //
 
 final class DotCaseDetector: CaseDetector {
-    
+
     var caseType: CaseType {
         return .dotCase
     }
 
     public func detect(_ input: String) -> Bool {
-        guard let separator = caseType.separator else { return false }
-        
-        let containsSeparator = input.contains(separator)
-        let isLowercaseOnly = input == input.lowercased()
-        let noOtherSeparators = !CaseType.containsOtherSeparators(in: input, excluding: caseType)
-        let hasValidCharacters = !input.filter { $0 != separator }.isEmpty
-        
-        return containsSeparator && isLowercaseOnly && noOtherSeparators && hasValidCharacters
+        return hasValidSeparatorStructure(input) && input == input.lowercased()
     }
 }
--- Sources/cases-tool/Protocol/CaseDetector.swift
+++ Sources/cases-tool/Protocol/CaseDetector.swift
@@ -6,3 +6,15 @@ public protocol CaseDetector {
     var caseType: CaseType { get }
     func detect(_ input: String) -> Bool
 }
+
+extension CaseDetector {
+    func hasValidSeparatorStructure(_ input: String) -> Bool {
+        guard let separator = caseType.separator else { return false }
+        
+        let containsSeparator = input.contains(separator)
+        let noOtherSeparators = !CaseType.containsOtherSeparators(in: input, excluding: caseType)
+        let hasValidCharacters = !input.filter { $0 != separator }.isEmpty
+        
+        return containsSeparator && noOtherSeparators && hasValidCharacters
+    }
+}
